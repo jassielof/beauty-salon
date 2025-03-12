55e091ca9bdb607b5352bf13caf4984c28f92177
Jassiel <a2021117600@estudiantes.upsa.edu.bo>
2025-03-12 18:55:09 -0400

    todo

--- beauty-salon/next.config.ts
+++ beauty-salon/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+// TODO: Fix vercel images not rendering/loading because the project is not the root directory of the repository, but rather in a subdirectory (/beauty-salon)
+// next.config.ts is in /beauty-salon as the images too, so I don't know what the issue is
 const nextConfig: NextConfig = {
   /* config options here */
 };
