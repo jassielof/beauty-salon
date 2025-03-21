27780f5c99052cf8cff3ba46bc37eeeb3b3b0bf3
Jassiel O. <jassielovando@protonmail.com>
2025-03-21 13:31:37 -0400

    clerk auth

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginLink>Log in</LoginLink>;
+    </div>
+  );
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+
+export default function Register() {
+  return (
+    <div>
+      <RegisterLink>Log in</RegisterLink>;
+    </div>
+  );
+}
