cba00306e38f3ce96c125cccde11f24b1631a3c5
Jassiel O. <jassielovando@protonmail.com>
2025-04-24 10:34:43 -0400

    bro

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
-pip install uv
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade uv || python3 -m pip install --upgrade pipx && pipx install uv
 uv sync
 uv run ../manage.py collectstatic --noinput
