9578e94784234d0919f319c4a2b84a144bd6d345
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-05-02 14:20:24 -0400

    files

--- beauty_salon/settings.py
+++ beauty_salon/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 if os.environ.get("DJANGO_ENV") == "production":
